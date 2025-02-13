cbe1be0cb5e627a9664aeaae2a972bd98d8c84f1
Sandhya Banavathi Suresh <banavathisuresh.s@northeastern.edu>
2025-02-12 19:43:52 -0500

    added websocket and event listener

---
+++ src/main/java/com/realTime/chatApp/chat/ChatMessage.java
@@ -0,0 +1,15 @@
+package com.realTime.chatApp.chat;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+
+    private String content;
+    private String sender;
+    private MessageType type;
+}
---
+++ src/main/java/com/realTime/chatApp/chat/MessageType.java
@@ -0,0 +1,8 @@
+package com.realTime.chatApp.chat;
+
+public enum MessageType {
+
+    CHAT,
+    JOIN,
+    LEAVE
+}
