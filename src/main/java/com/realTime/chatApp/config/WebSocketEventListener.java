6e53218c375493bf07920609a37cb741006d8e97
Sandhya Banavathi Suresh <banavathisuresh.s@northeastern.edu>
2025-02-12 22:33:11 -0500

    added frontend code and cleared lombok error

--- src/main/java/com/realTime/chatApp/config/WebSocketEventListener.java
+++ src/main/java/com/realTime/chatApp/config/WebSocketEventListener.java
@@ -3,32 +3,32 @@ package com.realTime.chatApp.config;
 import com.realTime.chatApp.chat.ChatMessage;
 import com.realTime.chatApp.chat.MessageType;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Component
 @RequiredArgsConstructor
-@Slf4j
 public class WebSocketEventListener {
 
+    private static final Logger log = LoggerFactory.getLogger(WebSocketEventListener.class);
     private final SimpMessageSendingOperations messageTemplate;
 
+    public WebSocketEventListener(SimpMessageSendingOperations messageTemplate) {
+        this.messageTemplate = messageTemplate;
+    }
+
     @EventListener
-    public void handleWebSocketDisconnectListener(
-            SessionDisconnectEvent event
-    ){
+    public void handleWebSocketDisconnectListener(SessionDisconnectEvent event){
         StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
         String username = (String) headerAccessor.getSessionAttributes().get("username");
         if(username != null){
             log.info("User Disconnected: {}", username);
-            var chatMessage = ChatMessage.builder()
-                    .type(MessageType.LEAVE)
-                    .sender(username)
-                    .build();
+            var chatMessage = ChatMessage.builder().type(MessageType.LEAVE).sender(username).build();
             messageTemplate.convertAndSend("/topic/public", chatMessage);
         }
     }
